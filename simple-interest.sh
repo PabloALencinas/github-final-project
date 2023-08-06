cc5951aa8ceb8c50e879f146c6c333a894e72d22
PabloLencinas <70404177+PabloALencinas@users.noreply.github.com>
2023-08-06 01:20:08 -0300

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # PabloALencinas
    # Input:
    # p, principal amount
    # t, time period in years
